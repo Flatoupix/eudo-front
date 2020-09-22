ae7d227a81c43f11b8c1caffed374b700d2de41a
Pierre Nowak <pnowak@eudonet.com>
2020-09-22 11:36:34 +0200

    initial commit

---
+++ ednFile.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-file-input :label="$attrs.label" v-bind="$attrs"> </v-file-input>
+</template>
+
+<script>
+import { ednRequired } from './mixins/ednRequired'
+import { ednVModel } from './mixins/ednVModel'
+
+export default {
+  inheritAttrs: false,
+  mixins: [ednRequired, ednVModel],
+}
+</script>
+
+<style lang='stylus' scoped></style>,
